f239ac19a766dbbf57f91168c7c151ffcf140b07
wrxqed-dev <wrxqedua@gmail.com>
2025-06-11 17:22:19 +0300

    1.0

---
+++ config.py
@@ -0,0 +1,14 @@
+api_id = 28636897 #get it in my.telegram.org/auth?to=apps
+api_hash = 'edbc6af7d15dc7a54cd5afe970b302e6' #get it in my.telegram.org/auth?to=apps
+bot_token = '7663257361:AAFyIxCtnm2Lgp8npxoEoiCiltwQHtLADPE' #get it by contacting to @BotFather
+
+balance_ton = "0" #initial balance TON of each user. Example: 1
+balance_usdt = "0" #initial balance USDT of each user. Example: 10
+
+admin_id = 7010880532 #admin telegram id, get it in @username_to_id_bot. Example: 5143703753
+
+bot_username = "sendsummersBot" #you bot @username
+support_bot_username = "sendsummersBot" #support bot @username, don't use offical support bot username (@TonRocketSupportBot)!! Example: xRockeetSupportBot
+
+delete_all_messages = True #delete all new users messages? True or False
+all_logs = True #print all logs? True or False
